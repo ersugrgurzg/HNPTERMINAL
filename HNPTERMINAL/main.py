9b37856b06e80c6b4ae7477b084f1c3216aad704
test <160679534+ersugrgurzg@users.noreply.github.com>
2024-07-14 21:41:06 +0200

    del

--- HNPTERMINAL/main.py
+++ HNPTERMINAL/main.py
@@ -1,16 +1,1 @@
-import os
-from rich.console import Console
-from rich.progress import track
-from rich.style import Style
-from rich.tree import Tree as Tr
-import datetime
-import shutil
-import psutil
-import requests
-import webbrowser
-import sys
-import subprocess
-import time
-from terminal import *
-            
-Terminale()
+# DELETED
